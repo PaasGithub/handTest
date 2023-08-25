dd993b25d21ba3c635639caf8eb433fa573cb903
PaasGithub <pkaddae56@gmail.com>
2023-08-25 22:08:14 +0200

    first commit

---
+++ handDetect_photo.py
@@ -0,0 +1,56 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
+from handFunctions import draw_landmarks_on_image
+
+# img = cv2.imread("handTest\handImage1.jpg")
+
+# Check if the image was loaded successfully
+""" if img is not None:
+    # Check if the image has valid dimensions
+    if img.shape[0] > 0 and img.shape[1] > 0:
+        cv2.imshow("chosen Image", img)
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()
+    else:
+        print("Image dimensions are not valid.")
+else:
+    print("Failed to load the image.") """
+
+# STEP 2: Create an HandLandmarker object.
+base_options = python.BaseOptions(model_asset_path='C:/Users/45818/Desktop/code/handTest/hand_landmarker.task')
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+originalimage = mp.Image.create_from_file("C:/Users/45818/Desktop/code/handTest/handImage1.jpg")
+
+#resize image parameters
+desired_width = 800  # Replace with your desired width
+desired_height = 600  # Replace with your desired height
+
+# Convert the mp.Image to a NumPy array
+image_data = originalimage.numpy_view()
+
+# Resize the image using cv2.resize
+resized_image_data = cv2.resize(image_data, (desired_width, desired_height))
+
+#resized_image 
+image = mp.Image(
+    image_format=mp.ImageFormat.SRGB,
+    data=resized_image_data
+)
+
+#new. just resize before beginning above
+
+# STEP 4: Detect hand landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the classification result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+
+cv2.imshow("chosen Image Boxed", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)  # Wait for a key press
+cv2.destroyAllWindows()
---
+++ handDetect_video.py
@@ -0,0 +1,103 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
+from handFunctions import draw_landmarks_on_image
+
+
+""" # Create a VideoCapture object to access the camera (usually 0 for built-in webcam)
+cap = cv2.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not access the camera.")
+    exit()
+
+while True:
+    # Read a frame from the camera
+    success, vidframe = cap.read()
+
+    # Check if the frame was successfully read
+    if not success:
+        print("Error: Could not read frame.")
+        break
+
+    # Display the frame in a window
+    cv2.imshow("Live Video Feed", vidframe)
+
+    # Break the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture and close the OpenCV windows
+cap.release()
+cv2.destroyAllWindows() """
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a hand landmarker instance with the live stream mode:
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
+    #print(".")
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='handTest/hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result
+)
+
+with HandLandmarker.create_from_options(options) as landmarker:
+    # Create a VideoCapture object to access the camera (usually 0 for built-in webcam)
+    cap = cv2.VideoCapture(0)
+
+    # Check if the camera opened successfully
+    if not cap.isOpened():
+        print("Error: Could not access the camera.")
+        exit()
+
+    while True:
+        # Read a frame from the camera
+        success, video = cap.read()
+        frame_timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+
+        # Check if the frame was successfully read
+        if not success:
+            print("Error: Could not read frame.")
+            break
+        
+        vidFrame = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=video
+        ) 
+        
+        #print ("this is vidframe:",vidFrame)
+
+        #Detect hand landmarks from the live feed.
+        detection_result = landmarker.detect_async(vidFrame, frame_timestamp_ms)
+        
+        print("this is;",detection_result)
+        #Process the classification result. In this case, visualize it.
+        #annotated_frame = draw_landmarks_on_image(vidFrame, detection_result)
+
+        # if detection_result is not None:
+        #     #annotated_frame = draw_landmarks_on_image(vidFrame, detection_result)
+        #     print("this is annotated frame")
+        # else:
+        #     print("No hand landmarks detected in this frame.")
+
+        # Display the frame in a window
+        #cv2.imshow("Live Video Feed", annotated_frame)
+        cv2.imshow("Live Video Feed", video)
+
+        # Break the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    # Release the VideoCapture and close the OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+    
