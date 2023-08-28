c7571686b06075f38380446f836040461c40d331
PaasGithub <pkaddae56@gmail.com>
2023-08-28 15:45:49 +0200

    combined hand detect and gesture. Abomination

---
+++ handGesture_patchedup.py
@@ -0,0 +1,114 @@
+import mediapipe as mp
+import cv2
+from handFunctions import draw_landmarks_on_image
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a gesture recognizer instance with the live stream mode:
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    for gesture in result.gestures:
+        #print([category.category_name for category in gesture])
+        print('Gesture type:{}'.format([category.category_name for category in gesture]))
+    
+    for handedness in result.handedness:
+        #print([category.category_name for category in gesture])
+        print('Hand type:{}'.format([category.category_name for category in handedness]))
+
+options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path='C:/Users/45818/Desktop/code/handTest/gesture_recognizer.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+timestamp = 0
+with GestureRecognizer.create_from_options(options) as recognizer:
+    # Initialize MediaPipe Hand Landmarker
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_drawing = mp.solutions.drawing_utils
+
+    # Create a VideoCapture object to access the camera (usually 0 for built-in webcam)
+    cap = cv2.VideoCapture(0)
+
+    # Check if the camera opened successfully
+    if not cap.isOpened():
+        print("Error: Could not access the camera.")
+        exit()
+
+    while True:
+        # Read a frame from the camera
+        success, video = cap.read()
+        frame_timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+
+        # Check if the frame was successfully read
+        if not success:
+            print("Error: Could not read frame.")
+            break
+
+        timestamp += 1
+        #print(timestamp)
+
+        # Convert the frame to RGB (MediaPipe uses RGB images)
+        frame_rgb = cv2.cvtColor( video, cv2.COLOR_BGR2RGB)
+
+        # Process the frame and detect hands
+        detectresults = hands.process(frame_rgb)
+
+        if detectresults.multi_hand_landmarks:
+            num_hands = len(detectresults.multi_hand_landmarks)  # Get the number of detected hands
+            print(f"Number of hands detected: {num_hands}")
+
+            vidFrame = mp.Image(
+                image_format=mp.ImageFormat.SRGB,
+                data=video
+            )
+            #print(vidFrame)
+
+            recognition_result = recognizer.recognize_async(vidFrame, timestamp)
+
+            for landmarks in detectresults.multi_hand_landmarks:
+                # Loop through landmarks and draw them on the frame
+                for landmark in landmarks.landmark:
+                    x, y, z = int(landmark.x * video.shape[1]), int(landmark.y * video.shape[0]), int(landmark.z * video.shape[1])
+                    cv2.circle(video, (x, y), 5, (0, 255, 0), -1)
+
+                # Draw hand landmarks on the frame
+                mp_drawing.draw_landmarks(video, landmarks, mp_hands.HAND_CONNECTIONS)
+
+        else:
+            print("noHands")
+
+        # vidFrame = mp.Image(
+        #     image_format=mp.ImageFormat.SRGB,
+        #     data=video
+        # )
+        # #print(vidFrame)
+
+        # recognition_result = recognizer.recognize_async(vidFrame, timestamp)
+        #print("this is recognition:", recognition_result)
+        #print(options)
+
+        # if recognition_result is not None:
+        #     print('detected')
+        #     annotated_frame = draw_landmarks_on_image(vidFrame, recognition_result)
+        #     #print("this is annotated frame")
+        #     cv2.imshow("Live Video Feed", annotated_frame)
+        # else:
+        #     #print("No hand landmarks detected in this frame.")
+        #     cv2.imshow("Live Video Feed", video)
+
+        # Display the frame in a window
+        #cv2.imshow("Live Video Feed", annotated_frame)
+        cv2.imshow("Live Video Feed", video)
+
+        # Break the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    # Release the VideoCapture and close the OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
